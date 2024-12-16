432007ff7cfa5abdbadc9c3949f17807cc44bef5
aktrs <174441696+aktrs@users.noreply.github.com>
2024-12-16 22:24:02 +0900

    修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
     attachment :image
     belongs_to :user
 
-    validates :age, presence: true, length: { maximum: 2}
+    validates :age, presence: true, format: { with: /\A[\x20-\x7E]*\z/, message: "には全角文字を含めないでください" }, length: { maximum: 2}
     validates :month, presence: true, length: {maximum: 2}
     validates :event, presence: true, length: {maximum: 10}
     validates_length_of :event_detail, maximum: 50, allow_nil: true
