2f86ed08c760ca53974ac5a6bc5d861b5dcbc6af
aktrs <174441696+aktrs@users.noreply.github.com>
2024-12-18 19:07:02 +0900

    修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,23 @@ class Event < ApplicationRecord
     attachment :image
     belongs_to :user
 
-    validate :age_cannot_contain_full_width_characters_and_be_a_number
-    validate :month_cannot_contain_full_width_characters_and_be_a_number
+    validate :age_must_be_half_width_digits
+    validate :month_must_be_half_width_digits
     validates :age, presence: true, length: { maximum: 2}
     validates :month, presence: true, length: {maximum: 2}
     validates :event, presence: true, length: {maximum: 10}
     validates_length_of :event_detail, maximum: 50, allow_nil: true
     
     private
-    def age_cannot_contain_full_width_characters_and_be_a_number
-        if age.to_i.to_s != age
-            errors.add(:age, "には全角文字を含めないでください")
-          end
+    def age_must_be_half_width_digits
+        unless age =~ /\A\d+\z/ 
+            errors.add(:age, "には半角数字のみを入力してください")
+        end
     end
 
-    def month_cannot_contain_full_width_characters_and_be_a_number
-        if month.to_i.to_s != month
-          errors.add(:month, "には全角文字を含めないでください")
+    def month_must_be_half_width_digits
+        unless month =~ /\A\d+\z/
+          errors.add(:month, "には半角数字のみを入力してください")
         end
     end
 end
