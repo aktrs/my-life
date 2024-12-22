7f1cc785495730dbb16e3788c4d361c3210c7dee
aktrs <174441696+aktrs@users.noreply.github.com>
2024-12-22 22:44:17 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
+
+  def create
+    if Entry.where(:user_id => current_user.id, room_id: params[:message][:room_id]).present?
+        @message = Message.new(message_params)
+
+        if @message.save
+          redirect_to "/rooms/#{@message.room_id}"
+        else
+          flash[:alert] = "メッセージの送信に失敗しました。"
+          redirect_back(fallback_location:root_path)
+        end
+        else
+          flash[:alert] = "トークルームが見つかりません。"
+          redirect_back(fallback_location:root_path)
+        end
+    end
+
+  private
+  def message_params
+      params.require(:message).permit(:message, :room_id).merge(user_id: current_user.id)
+  end   
 end
