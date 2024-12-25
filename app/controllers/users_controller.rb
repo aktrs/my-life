bcf5256b87a51e071240d18e6798964c5b6f851c
aktrs <174441696+aktrs@users.noreply.github.com>
2024-12-25 18:11:46 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,21 +18,23 @@ class UsersController < ApplicationController
     end
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
+    if @user.id != current_user.id
+       @isRoom = false
+       @currentUserEntry.each do |cu|
+         @userEntry.each do |u|
+         if cu.room_id == u.room_id
+           @isRoom = true
+           @roomId = cu.room_id
+          break
+         end
+       end
+       break if @isRoom
       end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
+    end
+
+    unless @isRoom
+     @room = Room.new
+     @entry = Entry.new
     end
   end
 
