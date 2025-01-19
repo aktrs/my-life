c270f32f85096a606e9b4f3bd879ec57a248e958
aktrs <174441696+aktrs@users.noreply.github.com>
2025-01-19 14:37:04 +0900

    グラフ数値の修正

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -7,6 +7,6 @@ class Graph < ApplicationRecord
     has_one_attached :image
 
     validates :age, presence: true, length: { maximum: 2 }, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 99 }, format: { with: /\A[0-9]+\z/ }
-    validates :value, presence: true, length: { maximum: 3 }, numericality: { greater_than_or_equal_to: -100, less_than_or_equal_to: 100 }, format: { with: /\A[0-9]+\z/ }
+    validates :value, presence: true, length: { maximum: 4 }, numericality: { greater_than_or_equal_to: -100, less_than_or_equal_to: 100 }, format: { with: /\A-?\d+\z/ }
 end
 
